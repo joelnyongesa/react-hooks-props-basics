5870627af00b8acdc44258520e81742041225eca
joelnyongesa <joelnyongesa148@gmail.com>
2023-08-18 12:57:09 +0300

    Welcome to props

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -9,7 +9,12 @@ function BlogPost() {
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+
+      <BlogContent articleText="Hello Joel, welcome to Props!"/>
+      <Comment commentText="Thanks Joel, I feel welcome here- Joel" />
+      <Comment commentText = "Yes, welcome to the club buddy!- Eunice Makena" />
     </div>
+
   );
 }
 
